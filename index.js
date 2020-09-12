d7bdd9d7d668a6cff5c9b5b6551ed93bfe10fda3
SWZ <contact@swz.works>
2020-09-12 16:06:18 +0200

    Added front-end, ejs rendering, reading pastes and other stuff

--- index.js
+++ index.js
@@ -1,62 +1,83 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const config = require('./config');
-const fs = require('fs');
-const randexp = require('randexp');
-const pasteman = require('./paste-manager');
+const bodyParser = require("body-parser");
+const config = require("./config");
+const fs = require("fs");
+const randexp = require("randexp");
+const pasteman = require("./paste-manager");
 
 const listener = app.listen(config.port, () => {
-    console.log(`Firepaste server is listening on port ${listener.address().port}!`);
-    setup();
-})
-
-app.use((req, res, next) => {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "*");
-    next();
+  console.log(
+    `Firepaste server is listening on port ${listener.address().port}!`
+  );
+  setup();
 });
 
-app.use(bodyParser.text({ type: 'text/*' }))
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
-app.post('/paste', (req, res) => {
-    let pasteID = genPasteID();
-        console.log(req.body)
-        fs.writeFile(`./pastes/${pasteID}.firepaste`, req.body, (err) => {
-            if(err) throw err;
-            res.send({success: true, data: {pasteID: pasteID}});
-            addPaste(app, `${pasteID}.firepaste`);
-        })
-})
+app.use(bodyParser.text({ type: "text/*" }));
 
-app.get('/', (req, res) => {
-    res.sendStatus(200)
-})
+app.use(express.static(__dirname + "/public"));
+
+app.post("/paste", (req, res) => {
+  let pasteID = genPasteID();
+  console.log(req.body);
+  fs.writeFile(`./pastes/${pasteID}.firepaste`, req.body, (err) => {
+    if (err) throw err;
+    res.send({ success: true, data: { pasteID: pasteID } });
+    addPaste(app, `${pasteID}.firepaste`);
+  });
+});
+
+app.get("/:paste", (req, res, next) => {
+  if (fs.existsSync(`./pastes/${req.params.paste}.firepaste`)) {
+    let data = fs.readFileSync(
+      `./pastes/${req.params.paste}.firepaste`,
+      "utf-8"
+    );
+    res.render(__dirname + "/public/index.ejs", { paste: data });
+  } else {
+    res.redirect("/");
+  }
+});
+app.get("/", (req, res, next) => {
+  res.render(__dirname + "/public/index.ejs", { paste: "" });
+});
 
 function setup() {
-    fs.readdirSync('./pastes/').forEach(pasteFile => {
-        if(!pasteFile.endsWith('.firepaste')) return;
+  fs.readdirSync("./pastes/").forEach((pasteFile) => {
+    if (!pasteFile.endsWith(".firepaste")) return;
 
-        addPaste(app, pasteFile);
-    })
+    addPaste(app, pasteFile);
+  });
 }
 
 function addPaste(app, pasteFile) {
-    app.get(`/${pasteFile.replace('.firepaste', '')}`, (req, res) => {
-        let content = fs.readFileSync(`./pastes/${pasteFile}/`, 'utf-8')
-        .split('\n').join('<br>')
+  app.get(`/${pasteFile.replace(".firepaste", "")}`, (req, res) => {
+    let content = fs
+      .readFileSync(`./pastes/${pasteFile}/`, "utf-8")
+      .split("\n")
+      .join("<br>");
 
-        res.send(content);
-    })
+    res.send(content);
+  });
 }
 
 function genPasteID() {
-    let regexp = /^[a-zA-Z1-9]{5}/;
-    let id = new randexp(regexp).gen();
+  let regexp = /^[a-zA-Z1-9]{5}/;
+  let id = new randexp(regexp).gen();
 
-    if(fs.existsSync(`./pastes/${id}.firepaste`)) return genPasteID();
+  if (fs.existsSync(`./pastes/${id}.firepaste`)) return genPasteID();
 
-    return id;
+  return id;
 }
--- public/script.js
+++ public/script.js
@@ -1,3 +1,34 @@
-function paste() {
-    
-}
+let editorLoaded = false;
+
+const newPaste = () => {
+  if (!editorLoaded) return;
+  document.body.classList.add("disabled");
+  document.querySelector("iframe").contentWindow.postMessage({
+    type: "get",
+  });
+};
+document.querySelector(".manageCard button").onclick = newPaste;
+
+document.querySelector("iframe").onload = () => {
+  editorLoaded = !editorLoaded;
+
+  if (pasteData) {
+    document.querySelector("iframe").contentWindow.postMessage({
+      type: "set",
+      data: pasteData,
+    });
+  }
+
+  window.addEventListener("message", async (m) => {
+    let req = await fetch("/paste", {
+      body: m.data,
+      headers: {
+        "Content-Type": "text/plain",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: "POST",
+    });
+    let res = JSON.parse(await req.text());
+    window.location.replace("/" + res.data.pasteID);
+  });
+};
